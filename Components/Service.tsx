47cb72b88af5d03d6108bf22d6441130ffed1a65
Rugved Dhorje <rdhorje6@gmail.com>
2025-01-06 19:53:47 +0530

    second commit

--- Components/Service.tsx
+++ Components/Service.tsx
@@ -5,6 +5,7 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 const Service = () => {
   const circleRef = useRef(null);
+  const textRef = useRef(null);
   const containerRef = useRef(null);
 
   useEffect(() => {
@@ -22,6 +23,16 @@ const Service = () => {
         scrub: true, // Smooth animation based on scroll position
       },
     });
+    gsap.to(textRef.current, {
+      scale: 90, // Final size multiplier
+      ease: "none", // Linear animation
+      scrollTrigger: {
+        trigger: textRef.current, // Element to watch for scroll
+        start: "50% center", // Start animation at the top of the page
+        end: "bottom top", // End animation when the container leaves the viewport
+        scrub: true, // Smooth animation based on scroll position
+      },
+    });
   }, []);
   return (
     <div>
@@ -29,7 +40,7 @@ const Service = () => {
         ref={containerRef}
         className="max-w-screen-2xl w-full mx-auto rounded-t-3xl pb-40 overflow-hidden"
       >
-        <h1 className="text-center text-[90px] font-bold text-black leading-none pt-[200px] pb-[150px]">
+        <h1 className="text-center text-[90px] font-bold text-gray-700 leading-none pt-[200px] pb-[150px]">
           Embrace the future of <br /> urban mobility
         </h1>
         <div className="w-10/12 mx-auto flex flex-wrap mb-10 relative">
@@ -37,8 +48,12 @@ const Service = () => {
           <div className="absolute inset-0 flex items-center justify-center">
             <div
               ref={circleRef}
-              className="w-14 h-14 bg-gray-700 rounded-full"
-            ></div>
+              className="w-14 h-14 bg-gray-700 rounded-full flex justify-center items-center"
+            >
+              <h1 ref={textRef} className="text-white">
+                minimal
+              </h1>
+            </div>
           </div>
 
           {/* Content Divs */}
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
-"use client";
+// "use client";
 import HomePage from "@/Components/HomePage";
 import Service from "@/Components/Service";
-import { useEffect } from "react";
+// import { useEffect } from "react";
 
 export default function Home() {
-  useEffect(() => {
-    (async () => {
-      const LocomotiveScroll = (await import("locomotive-scroll")).default;
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      const locomotiveScroll = new LocomotiveScroll();
-    })();
-  }, []);
+  // useEffect(() => {
+  //   (async () => {
+  //     const LocomotiveScroll = (await import("locomotive-scroll")).default;
+  //     // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  //     const locomotiveScroll = new LocomotiveScroll();
+  //   })();
+  // }, []);
   return (
     <main>
       <HomePage />
